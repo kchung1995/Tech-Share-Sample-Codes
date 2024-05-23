036ef16357b7f857af56999e4383c94d579e1b16
Kuyho Chung <kchung1995@gmail.com>
2024-05-23 23:46:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/katfun/tech/share/sample/codes/TechShareSampleCodesApplication.kt
@@ -0,0 +1,11 @@
+package com.katfun.tech.share.sample.codes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechShareSampleCodesApplication
+
+fun main(args: Array<String>) {
+    runApplication<TechShareSampleCodesApplication>(*args)
+}
---
+++ src/test/kotlin/com/katfun/tech/share/sample/codes/TechShareSampleCodesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.katfun.tech.share.sample.codes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechShareSampleCodesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
